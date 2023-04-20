645fbbb2eb1a4f07e23ba343ffa3b8d6338cae86
filipe-oliv95 <filipec.oliveira95@gmail.com>
2023-04-20 16:23:03 -0300

    Correção na classe Menu

--- src/menus/Menu.java
+++ src/menus/Menu.java
@@ -1,25 +1,172 @@
 package menus;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 import contas.Conta;
 import contas.ContaCorrente;
+import pessoas.Cliente;
+import pessoas.Funcionario;
+import pessoas.enums.UsuariosEnum;
 import principal.SistemaBancario;
 import relatorios.Relatorio;
 
 public class Menu {
 
-	public static void imprimirMenuCliente(Conta conta) {
+	public static void menuInicial() {
+
+		Scanner sc = new Scanner(System.in);
+		Cliente cliente;
+		Conta conta;
+		// Usuario usuario;
+		Funcionario funcionario;
+		int senha = 0;
+		do {
+			System.out.println();
+			System.out.println("================================");
+			System.out.println("=== BEM VINDO AO BANCO JAVA ====");
+			System.out.print("[ Digite seu CPF ]:");
+			String cpf = sc.nextLine(); // CPF do usuário
+			System.out.print("[ Digite sua senha ]:");
+			senha = sc.nextInt();
+			sc.nextLine();
+			System.out.println("================================");
+
+			cliente = SistemaBancario.mapaDeClientes.get(cpf);
+			conta = SistemaBancario.mapaDeContas.get(cpf);
+			funcionario = SistemaBancario.mapaDeFuncionarios.get(cpf);
+			List<Conta> listaContas = new ArrayList<>(SistemaBancario.mapaDeContas.values());
+
+			if (conta != null && funcionario != null) {
+				if (funcionario.getSenha() == senha) {
+					menuFuncionario(funcionario, conta, listaContas, cpf, cliente);
+				}
+			} else if (conta != null && cliente != null) {
+				if (cliente.getSenha() == senha) {
+					System.out.println("Cliente");
+					Menu.menuCliente(conta, cliente);
+				} else {
+					System.out.println("DADOS INCORRETOS. Digite novamente \n");
+				}
+			}
+		} while (conta == null || cliente == null || cliente.getSenha() != senha || funcionario.getSenha() != senha);
+
+		sc.close();
+
+	}
+
+	public static void menuFuncionario(Funcionario funcionario, Conta conta, List<Conta> listaContas, String cpf, Cliente cliente) {
+		Scanner sc = new Scanner(System.in);
+
+		int opcao = 0;
+		System.out.println();
+		System.out.println("Olá " + funcionario.getTipoDeUsuario().getTipoPessoa());
+		do {
+			System.out.println("Escolha entre as opções abaixo:");
+			System.out.println("[1] Entrar como cliente");
+			System.out.println("[2] Entrar como " + funcionario.getTipoDeUsuario().getTipoPessoa());
+			System.out.println("[3] Logout");
+			opcao = sc.nextInt();
+
+			switch (opcao) { // MENU CLIENTE PARA FUNCIONARIO
+
+			case 1:
+				Menu.menuCliente(conta, cliente);
+				break;
+			case 2: // MENU DO FUNCIONÁRIO
+				switch (funcionario.getTipoDeUsuario()) {
+				case GERENTE:
+					System.out.println();
+					System.out.println("Escolha entre as opções abaixo:");
+					System.out.println("[1] Consulta de contas da agência " + conta.getAgencia().getNumAgencia());
+					System.out.println("[2] Retornar ao menu anterior");
+
+					switch (opcao) {
+					case 1:
+						Relatorio.numDeContasNaAgencia(cpf);
+						break;
+					case 2:
+						menuFuncionario(funcionario, conta, listaContas, cpf, cliente);
+						break;
+					}
+					break;
+
+				case DIRETOR:
+					System.out.println();
+					System.out.println("Escolha entre as opções abaixo:");
+					System.out.println("[1] Consulta de contas por agência");
+					System.out.println("[2] Relatório de informações dos clientes do banco");
+					System.out.println("[3] Retornar ao menu anterior");
+					opcao = sc.nextInt();
+					switch (opcao) {
+					case 1:
+						Relatorio.numDeContasNaAgencia(cpf);
+						break;
+					case 2:
+						Relatorio.informacoesClientes(listaContas);
+						break;
+					case 3:
+						menuFuncionario(funcionario, conta, listaContas, cpf, cliente);
+						break;
+					default:
+						Menu.menuCliente(conta, cliente);
+						break;
+					}
+					break;
+				case PRESIDENTE:
+					System.out.println();
+					System.out.println("Escolha entre as opções abaixo:");
+					System.out.println("[1] Consulta de contas por agência");
+					System.out.println("[2] Relatório de informações dos clientes do banco");
+					System.out.println("[3] Relatório valor total armazenado");
+					System.out.println("[4] Retornar ao menu anterior");
+					opcao = sc.nextInt();
+					switch (opcao) {
+					case 1:
+						Relatorio.numDeContasNaAgencia(cpf);
+						break;
+					case 2:
+						Relatorio.informacoesClientes(listaContas);
+						break;
+					case 3:
+						System.out.println("CRIAR RELATORIO DO CAPITAL ARMAZENADO");
+						break;
+					case 4:
+						menuFuncionario(funcionario, conta, listaContas, cpf, cliente);
+						break;
+					default:
+						Menu.menuCliente(conta, cliente);
+						break;
+					}
+					break;
+				default:
+					break;
+				}
+			case 3:
+				menuInicial();
+				break;
+			}
+		} while (!(opcao == 0)); // PENSAR EM UMA OUTRO SOLUÇÃO
+
+		sc.close();
+
+	}
+
+	public static void menuCliente(Conta conta, Cliente cliente) {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 
+		System.out.println();
+		System.out.println("Olá " + cliente.getNome() + "!");
 		System.out.println("Escolha entre as opções abaixo:");
 		System.out.println("[1] Saque");
 		System.out.println("[2] Depósito");
 		System.out.println("[3] Transferência para outra conta");
 		System.out.println("[4] Extrato da conta");
 		System.out.println("[5] Relatórios");
+		System.out.println("[6] Logout");
 
 		int opcao = sc.nextInt();
 		// adicionar o retorno
@@ -58,41 +205,48 @@ public class Menu {
 		case 5:
 			menuRelatorio(conta);
 			break;
+		case 6:
+			menuInicial();
 		default:
 			break;
 		}
 		sc.close();
 	}
-	
+
 	public static void menuRelatorio(Conta conta) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("[1] Saldo");
 		System.out.println("[2] Relatório de tributação da conta corrente");
 		System.out.println("[3] Relatório de Rendimento da poupança");
 		System.out.println("[4] Desafio");
-		
-		int opcao = 0; 
-		
+		System.out.println("[5] Logout");
+
+		int opcao = 0;
+
 		do {
 			opcao = sc.nextInt();
-			
+
 			switch (opcao) {
 			case 1:
 				Relatorio.imprimirSaldo(conta);
 				break;
-			case 2:	
+			case 2:
 				Relatorio.tributacaoCC((ContaCorrente) conta);
 				break;
-			case 3: 
+			case 3:
 				Relatorio.simularRendimentoPoupanca();
 				break;
-			case 4: System.out.println("DESAFIO");
+			case 4:
+				System.out.println("DESAFIO");
 			default:
+			case 5:
+				menuInicial();
 				break;
 			}
-		}while(opcao < 1 || opcao > 4);
-		
+		} while (opcao < 1 || opcao > 4);
+
 		sc.close();
-	}	
+	}
+
 }
--- src/principal/SistemaBancario.java
+++ src/principal/SistemaBancario.java
@@ -35,7 +35,6 @@ public class SistemaBancario {
 
 		Leitor.leitura(".\\database\\registrodedados.txt");
 
-		System.out.println("TESTANDO LEITURA");
 
 		System.out.println("Testando Mapa Funcionários =>  " + mapaDeClientes.get("56489723114"));
 		System.out.println(mapaDeFuncionarios.get("80466528906"));
@@ -45,108 +44,9 @@ public class SistemaBancario {
 //			System.out.println(c);
 //		}
 
-		Cliente cliente;
-		Conta conta;
-		// Usuario usuario;
-		Funcionario funcionario;
-		int senha = 0;
-		do {
-			System.out.println();
-			System.out.println("================================");
-			System.out.println("=== BEM VINDO AO BANCO JAVA ====");
-			System.out.print("[ Digite seu CPF ]:");
-			String cpf = sc.nextLine(); // CPF do usuário
-			System.out.print("[ Digite sua senha ]:");
-			senha = sc.nextInt();
-			sc.nextLine();
-			System.out.println("================================");
-
-			cliente = mapaDeClientes.get(cpf);
-			conta = mapaDeContas.get(cpf);
-			// usuario = mapaDeUsuarios.get(cpf);
-			funcionario = mapaDeFuncionarios.get(cpf);
-			List<Conta> listaContas = new ArrayList<>(mapaDeContas.values());
-
-			int opcao = 0;
-			if (conta != null && funcionario != null) {
-				if (funcionario.getSenha() == senha) {
-					System.out.println("Olá " + funcionario.getTipoDeUsuario());
-					do {
-						System.out.println("Deseja entrar na conta de Cliente [1] ou Funcionario [2]");
-						opcao = sc.nextInt();
-
-						switch (opcao) { // MENU CLIENTE PARA FUNCIONARIO
-						case 0:
-							break;
-						case 1:
-							System.out.println("MENU CLIENTE para FUNCIONARIO");
-							Menu.imprimirMenuCliente(conta);
-							break;
-						case 2: // MENU DO FUNCIONÁRIO
-							switch (funcionario.getTipoDeUsuario()) {
-							case GERENTE:
-								System.out.println("Deseja consultar a lista de contas em sua agência ? S/N ");
-								char op = sc.next().charAt(0);
-
-								if (op == 'S' || op == 's') {
-									Relatorio.numDeContasNaAgencia(cpf); // pegou List<Conta>
-								} else {
-									Menu.imprimirMenuCliente(conta);
-								}
-								break;
-							case DIRETOR:
-								System.out.println("[1] Consulta de contas por agência");
-								System.out.println("[2] Relatório de informações dos clientes do banco");
-								opcao = sc.nextInt();
-								switch (opcao) {
-								case 1:
-									Relatorio.numDeContasNaAgencia(cpf);
-									break;
-								case 2:
-									Relatorio.informacoesClientes(listaContas);
-									break;
-								default:
-									Menu.imprimirMenuCliente(conta);
-									break;
-								}
-								break;
-							case PRESIDENTE:
-								System.out.println("[1] Consulta de contas por agência");
-								System.out.println("[2] Relatório de informações dos clientes do banco");
-								System.out.println("[3] Relatório valor total armazenado");
-								opcao = sc.nextInt();
-								switch (opcao) {
-								case 1:
-									Relatorio.numDeContasNaAgencia(cpf);
-									break;
-								case 2:
-									Relatorio.informacoesClientes(listaContas);
-									break;
-								case 3:
-									System.out.println("CRIAR RELATORIO DO CAPITAL ARMAZENADO");
-								default:
-									Menu.imprimirMenuCliente(conta);
-									break;
-								}
-								break;
-							default:
-								break;
-							}
-						}
-					} while (!(opcao == 0)); // PENSAR EM UMA OUTRO SOLUÇÃO
-				} else {
-					System.out.println("DADOS INCORRETOS. Digite novamente \n");
-				}
-			} else if (conta != null && cliente != null) {
-				if (cliente.getSenha() == senha) {
-					System.out.println("Cliente");
-					Menu.imprimirMenuCliente(conta);
-				} else {
-					System.out.println("DADOS INCORRETOS. Digite novamente \n");
-				}
-			}
-		} while (conta == null || cliente == null || cliente.getSenha() != senha || funcionario.getSenha() != senha);
-
+		Menu.menuInicial();
+		
+		
 		sc.close();
 	}
 }
